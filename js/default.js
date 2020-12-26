f22ee727d5a690f9c141a88782f647f695e7a737
Igor Alentev <m8dotpie@Igors-MacBook-Pro.local>
2020-12-27 00:37:16 +0300

    js path fix

--- js/default.js
+++ js/default.js
@@ -2,8 +2,7 @@
 // Add class "active" to the button representing current page.
 function activateTab() {
     thisPage = location.pathname;
-    console.log(thisPage)
-    if (thisPage == "/" || thisPage.includes("index.html")) {
+    if (thisPage == "/" || thisPage == "/m8blog/" || thisPage.includes("index.html")) {
         (document.getElementById("tab0").className) = "active";
         return;
     }
